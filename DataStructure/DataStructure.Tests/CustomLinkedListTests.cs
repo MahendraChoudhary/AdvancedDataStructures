54a080c6b6ad4229c3cc0b36b986649ac8f0c2ef
Choudhary, Mahendra <mahendrachoudhary1008@gmail.com>
2021-01-11 22:32:52 +0530

    LinkedList DS

---
+++ DataStructure/DataStructure.Tests/CustomLinkedListTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Tests
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class CustomLinkedListTests
+    {
+        [TestMethod]
+        public void InsertData()
+        {
+            var linkList = new CustomLinkedList<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                linkList.Insert(i);
+            }
+
+            var items = linkList.Traverse();
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(i, items.ElementAt(i));
+            }
+        }
+    }
+}
---
+++ DataStructure/DataStructure/CustomLinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class CustomLinkedList<T>
+    {
+        private Node<T> head;
+
+        public void Insert(T value)
+        {
+            var node = new Node<T>(value);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                var current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = node;
+            }
+        }
+
+        public IEnumerable<T> Traverse()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                var tmp = current.CurrentValue;
+                current = current.Next;
+                yield return tmp;
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        private T currentValue;
+
+        public Node()
+        {
+            currentValue = default(T);
+        }
+
+        public Node(T value)
+        {
+            currentValue = value;
+        }
+
+        public Node<T> Next { get; set; }
+
+        public T CurrentValue => currentValue;
+    }
+}
